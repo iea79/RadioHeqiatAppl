6190c517b810ee4ea98c0b80a277bd592d0c238c
Evgeny Ivanov <busforward@gmail.com>
2025-04-09 20:29:45 +0500

    Update

--- index.js
+++ index.js
@@ -4,19 +4,18 @@
  *
  * @format
  * @flow strict-local
-*/
+ */
 import React from 'react';
-import { AppRegistry } from 'react-native';
-import { Provider } from 'react-redux';
+import {AppRegistry} from 'react-native';
+import {Provider} from 'react-redux';
 import App from './src/App';
 import {name as appName} from './app.json';
 import store from './src/store';
 
-
 const AppRedux = () => (
-    <Provider store={ store }>
-        <App />
-    </Provider>
-)
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
 
 AppRegistry.registerComponent(appName, () => AppRedux);
